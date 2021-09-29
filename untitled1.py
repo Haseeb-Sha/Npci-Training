371ff07c8743d14db08f8d87ff8022c20fde0c1f
Haseeb-Sha <91605865+Haseeb-Sha@users.noreply.github.com>
2021-09-29 17:16:56 +0530

    Delete untitled1.py

--- untitled1.py
+++
@@ -1,9 +0,0 @@
-l=[["surbhi","surbhi@gmail.com",31123],["sejal","sejal@gmail.com",132312],
-   ["akanksha","abs@gmail.com",52312],["auav","garav@gmail.com",43212]]
-
-with open('sample.txt','w') as file:
-    for i in l:
-        file.write(str(str(i)+"\n"))
-
-file2=open('sample.txt','r')
-file2.read()
