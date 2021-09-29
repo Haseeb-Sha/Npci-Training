d17f7ab6633e65e6738f40a6a1aab0e0b7c9db43
Haseeb-Sha <abdulhaseebshah3@gmail.com>
2021-09-29 17:59:01 +0530

    commit

---
+++ Python/Code 1.py
@@ -0,0 +1,30 @@
+x=5
+y=6
+i=input()
+if i=="sum":
+    sum=lambda x,y:x+y
+    print(sum(x,y))
+if i=="subtract":
+    subtract=lambda x,y:x-y
+    print(subtract(x,y))
+if i=="multi":
+    multi=lambda x,y:x*y
+    print(multi(x,y))
+if i=="divide":
+    divide=lambda x,y:x//y
+    print(divide(x,y))
+ls=[]
+print("Enter no of  values in list")
+n=int(input())
+for i in range (n):
+    a=input()
+    ls.append(a)
+print("inputted list",ls)
+
+lst_dct={ls[i]:ls[i+1] for i in range(0 , n ,2)}
+print("list converted to dict",lst_dct)
+
+for i in sorted (lst_dct):
+    print(i,lst_dct[i])
+
+    
--- code 3.py
+++
@@ -1,9 +0,0 @@
-l=[["haseeb","123@gmail.com",31123],["prakahr","345@gmail.com",132312],
-   ["suhail","abs@gmail.com",52312],["azaan","kbc\@gmail.com",43212]]
-
-with open('sample.txt','w') as file:
-    for i in l:
-        file.write(str(str(i)+"\n"))
-
-file2=open('sample.txt','r')
-file2.read()
